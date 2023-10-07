fe86ced8494000cd897555aa25903a73c7681c98
MrBabon <christophedanna06@gmail.com>
2023-10-07 15:19:08 +0200

    webpack ok

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,8 +6,14 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
+import ButtonPhoneController from "./button_phone_controller"
+application.register("button-phone", ButtonPhoneController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
+
+import ScrollController from "./scroll_controller"
+application.register("scroll", ScrollController)
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,6 @@
 class Organization < ApplicationRecord
+  has_many :events
+
   include PgSearch::Model
   pg_search_scope :search_by_organization,
                   against: [:name, :category],
